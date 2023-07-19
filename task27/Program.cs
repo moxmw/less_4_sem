0622cfc64f61c063a3b1efe2b91bf4434b364320
moxmw <138039933+moxmw@users.noreply.github.com>
2023-07-19 13:09:04 +0400

    Add files via upload

---
+++ task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int sum = 0;
+int remainder;
+
+while (num != 0)
+{
+    remainder = num % 10;
+    sum += remainder;
+    num /= 10;
+}
+
+Console.WriteLine("Сумма цифр в числе: " + sum);
---
+++ task29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = GenerateArray();
+
+Console.WriteLine("Массив элементов:");
+PrintArray(array);
+
+
+static int[] GenerateArray()
+{
+    int[] array = new int[8];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100);
+    }
+    return array;
+}
+
+static void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{String.Join(", ", array)}]");
+}
